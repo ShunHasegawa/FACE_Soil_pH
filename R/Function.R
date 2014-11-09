b33cb2fe331bde28fd301e949f5e73a04397d0c4
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-09 21:17:58 +0000

    co2 -> CO2

--- R/Function.R
+++ R/Function.R
@@ -9,16 +9,16 @@ subsetD <- function(...) droplevels(subset(...))
 # log OR sqrt OR power(1/3) OR inverse OR box-cox
 bxplts <- function(value, ofst = 0, data, ...){
   data$y <- data[[value]] + ofst #ofst is added to make y >0
-  a <- boxcox(y ~ co2 * time, data = data)
+  a <- boxcox(y ~ CO2 * time, data = data)
   par(mfrow = c(2, 3))
-  boxplot(y ~ co2*time, data, main = "raw")
-  boxplot(log(y) ~ co2*time, main = "log", data)
-  boxplot(sqrt(y) ~ co2*time, main = "sqrt", data)
-  boxplot(y^(1/3) ~ co2*time, main = "power(1/3)", data)
-  boxplot(1/y ~ co2*time, main = "inverse", data)
+  boxplot(y ~ CO2*time, data, main = "raw")
+  boxplot(log(y) ~ CO2*time, main = "log", data)
+  boxplot(sqrt(y) ~ CO2*time, main = "sqrt", data)
+  boxplot(y^(1/3) ~ CO2*time, main = "power(1/3)", data)
+  boxplot(1/y ~ CO2*time, main = "inverse", data)
   BCmax <- a$x[a$y == max(a$y)]
   texcol <- ifelse(BCmax < 0, "red", "black") 
-  boxplot(y^(BCmax) ~ co2*time, 
+  boxplot(y^(BCmax) ~ CO2*time, 
           main = "", sep = "=", 
           data = data)
   title(main = paste("Box Cox", round(BCmax, 4)), 
