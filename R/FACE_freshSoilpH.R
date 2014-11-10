376a24d0dd48ac16fc9900f5e006bc87cdce4ef1
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-10 01:05:08 +0000

    fig for ratio and also bar graph are created

--- R/FACE_freshSoilpH.R
+++ R/FACE_freshSoilpH.R
@@ -77,8 +77,13 @@ DepMean$time <- DepMean$year
 bxplts(value = "M", data = DepMean)
 m1 <- lmer(M ~ co2 * year + (1|block) + (1|ring), data = DepMean)
 Anova(m1)
-Anova(m1, test.statistic = "F")
+AnvF_pH <- Anova(m1, test.statistic = "F")
+AnvF_pH
 
+# create stat summary table for LMM with CO2 and time
+Stat_CO2Time <- StatTable(AnvF_pH)
+
+# model diagnosis
 plot(allEffects(m1))
 plot(m1)
 qqnorm(residuals(m1))
@@ -97,6 +102,10 @@ cntrst<- contrast(lmeMod,
 #######
 # Fig #
 #######
+
+###########
+## Ratio ##
+###########
 # Calculate ration e/a
 ## Add block
 RngMean$block <- recode(RngMean$ring, "c(1, 2) = 'A'; c(3, 4) = 'B'; c(5, 6) = 'C'")
@@ -116,7 +125,72 @@ p <- ggplot(data = RatMeanDF, aes(x = year, y = Mean))
 p + geom_bar(fill = "white", col = "black", stat = "identity") +
   geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE), width = .5) +
   coord_cartesian(ylim=c(.9, 1.1))
-?geom_errorbar
 
-?geom_bar
+##########
+## Mean ##
+##########
 
+# Add P values from the above contrast
+pvalsDF <- data.frame(year = c("2012", "2013"), 
+                      pval = paste("italic(P)==", c(0.62, "bold(0.09)"), sep = ""))
+
+CO2Mean <- merge(CO2Mean, pvalsDF, by = "year", all = TRUE)
+
+# Add ymin and ymax to draw lines which connect two bars where I place
+# significace symbols
+
+CO2Mean <- ddply(CO2Mean, .(year), transform, 
+                ymax = max(Mean + SE) + 0.1) 
+## common max values between co2 treatment for each year
+CO2Mean$ymin <- with(CO2Mean, (Mean + SE) +0.05)
+
+
+# df for stat table
+statDF <- StatPositionDF(StatRes = Stat_CO2Time, ytop = 6, ylength = 1, gap = .04)
+
+# graph setting
+science_theme <- theme(panel.grid.major = element_blank(),
+                       panel.grid.minor = element_blank(),
+                       legend.position = c(.15, .89), 
+                       legend.title = element_blank())
+
+# create a plot
+p <- ggplot(data = CO2Mean, aes(x = year, y = Mean, fill = co2))
+p2 <- p + geom_bar(stat = "identity", 
+                   position = position_dodge(width = .9), 
+             alpha = .7, 
+             col = "black") +
+  scale_fill_manual(values = c("black", "white"), 
+                    labels = c("Ambient", expression(eCO[2]))) +
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
+                position = position_dodge(width = .9), 
+                width = .5) +
+  scale_x_discrete(labels = c(expression(2012~(Pre-CO[2])), "2013")) +
+  coord_cartesian(ylim=c(5, 6)) +
+  labs(x = "Year", y = "Soil pH at 0-30 cm") +
+  science_theme +
+  # here draw lines which connect two bars for each year
+  geom_errorbar(aes(ymin = ymin, ymax = ymax),
+                   position = position_dodge(width = .9),
+                   width = 0) +
+  geom_segment(aes(x = year, xend = year, y = ymax, yend = ymax),
+               position = position_dodge(width = .9)) +
+  # here add P values
+  geom_text(aes(x = year, y = ymax, label = pval), 
+            parse = TRUE, vjust = -1, size = 2.5) +
+  # here add stat table
+  geom_text(data = subset(statDF, predictor != ""), 
+                aes(x = 2.3, y = yval, label = predictor),
+                size = 2, , hjust = 1, parse = TRUE) +
+  # unless remove [" "] with predictor != "", labels will be messed up due to
+  # this empty level
+  geom_text(data = statDF, 
+            aes(x = 2.4, y = yval, label = p), 
+            size = 2, parse = TRUE)
+  
+# Use the combination of geom_segment and geom_errorbar to connect to bars where
+# you place significant symbols. geom_segment doesn't work well with
+# position_dodge(). It makes x position dodged but not y. so when you need to
+# use position_dodge() (e.g. plotting barplot), use geom_errorbar to get
+# vertical lines
+ggsavePP(filename = "Output//Fig/Manuscript/FACE_SoilpH", plot = p2, width = 4, height = 4)
